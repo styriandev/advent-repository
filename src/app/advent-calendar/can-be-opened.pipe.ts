10089e639f954bf44a1ef77be63a1feb1993abfb
styriandev <dominiksammer@inode.at>
2022-10-26 21:49:16 +0200

    Added entries

--- src/app/advent-calendar/can-be-opened.pipe.ts
+++ src/app/advent-calendar/can-be-opened.pipe.ts
@@ -1,11 +1,12 @@
 import {Pipe, PipeTransform} from '@angular/core';
 import {Entry} from './entry';
+import {environment} from '../../environments/environment';
 
 @Pipe({
   name: 'canBeOpened'
 })
 export class CanBeOpenedPipe implements PipeTransform {
   transform(value: Entry): boolean {
-    return true;
+    return environment.openable || value.date < new Date();
   }
 }
